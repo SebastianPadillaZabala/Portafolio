040e6586256637a1b78b425f5301650311a4a456
SebastianPadillaZabala <padillasebastian140@gmail.com>
2024-01-10 18:05:26 -0400

    perf(frontend): Change in animation

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,26 @@
 const darkButton = document.querySelector('#darkButton');
+const iconElement = darkButton.querySelector('i');
 
 const darkButtonSmall = document.querySelector('#darkButtonSmall');
 
-darkButton.addEventListener('click', function() {
+darkButton.addEventListener('click', function () {
 
-    // if set via local storage previously
+    iconElement.classList.toggle('bi-moon-stars-fill');
+    iconElement.classList.toggle('bi-sun-fill');
+
+   
+    if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+    } else {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+    }
+});
+
+darkButtonSmall.addEventListener('click', function () {
+
+    
     if (localStorage.getItem('color-theme')) {
         if (localStorage.getItem('color-theme') === 'light') {
             document.documentElement.classList.add('dark');
@@ -14,7 +30,6 @@ darkButton.addEventListener('click', function() {
             localStorage.setItem('color-theme', 'light');
         }
 
-    // if NOT set via local storage previously
     } else {
         if (document.documentElement.classList.contains('dark')) {
             document.documentElement.classList.remove('dark');
@@ -24,43 +39,42 @@ darkButton.addEventListener('click', function() {
             localStorage.setItem('color-theme', 'dark');
         }
     }
-    
+
 });
 
-darkButtonSmall.addEventListener('click', function() {
+const scrollLeftButton = document.getElementById("scrollLeftButton");
+const scrollRightButton = document.getElementById("scrollRightButton");
 
-    // if set via local storage previously
-    if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        }
+const scrollContainer = document.querySelector(".overflow-x-auto");
 
-    // if NOT set via local storage previously
-    } else {
-        if (document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        } else {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
+scrollLeftButton.addEventListener("click", () => {
+    scrollContainer.scrollLeft -= 250; 
+});
+
+scrollRightButton.addEventListener("click", () => {
+    scrollContainer.scrollLeft += 250; 
+});
+
+function handleIntersection(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.visibility = 'visible';
+            entry.target.classList.add('fall-animation');
+            observer.unobserve(entry.target); 
         }
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
+
+const section = document.querySelector('.fall-animation');
+observer.observe(section);
+
+
+document.addEventListener('visibilitychange', function () {
+    if (document.visibilityState === 'hidden') {
+        section.classList.remove('fall-animation');
+        section.style.visibility = 'hidden';
+        observer.observe(section);
     }
-    
 });
-
-  const scrollLeftButton = document.getElementById("scrollLeftButton");
-  const scrollRightButton = document.getElementById("scrollRightButton");
-
-  const scrollContainer = document.querySelector(".overflow-x-auto");
-
-  scrollLeftButton.addEventListener("click", () => {
-    scrollContainer.scrollLeft -= 250; // Puedes ajustar la cantidad de desplazamiento
-  });
-
-  scrollRightButton.addEventListener("click", () => {
-    scrollContainer.scrollLeft += 250; // Puedes ajustar la cantidad de desplazamiento
-  });
